275e79aa0519202d96f66895c4923ca5968db272
Diogo <diogo_araujo9@hotmail.com>
2019-01-06 23:24:58 -0200

    Initial commit

---
+++ app/routes/login_routes.js
@@ -0,0 +1,73 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {    
+  app.get('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+
+      db.collection('notes').findOne(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send(item);
+        }
+      });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      const note = { text: req.body.body, title: req.body.title };
+      db.collection('notes').update(details, note, (err, result) => {
+        if (err) {
+            res.send({'error':'An error has occurred'});
+        } else {
+            res.send(note);
+        } 
+      });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      db.collection('notes').remove(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send('Note ' + id + ' deleted!');
+        } 
+      });
+  });
+
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.post('/login/username', (req, res) => {
+    let body = req.body;
+    const details = { email: body.email, password: body.password };
+    console.log(details);
+    db.collection('accounts').findOne(details, (err, item) => {  
+      res.send(item);
+    });
+  });
+
+  app.post('/user', (req, res) => {
+    let body = req.body;
+    const account = { email: body.email, password: body.password };
+    db.collection('accounts').insert(account, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb://diogo:k5t3i8l2@ds041924.mlab.com:41924/react-login-v2'
+  };
